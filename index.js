4522d1de8ba2237a034399ef42e061ac80b7c09e
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-06-17 17:58:02 +0100

    Fixed incorrect variable name.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-
 const app = express()
 
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 const port = process.env.PORT || 5000
-app.listen(PORT)
+app.listen(port)
