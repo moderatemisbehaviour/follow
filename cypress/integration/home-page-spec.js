8ab75645c0462237fc9d7198b6eaae0243c58038
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-06-19 08:40:02 +0100

    Search box is focused on page load.

--- cypress/integration/home-page-spec.js
+++ cypress/integration/home-page-spec.js
@@ -1,4 +1,4 @@
-describe('The home page.', function() {
+describe('Landing on the home page.', function () {
   beforeEach(function () {
     cy.visit('/')
   })
@@ -9,5 +9,15 @@ describe('The home page.', function() {
 
   it('Displays a search box.', function () {
     cy.get('#search')
+  })
+
+  it('Focuses the search box.', function () {
+    cy.get('#search').focused()
+  })
+})
+
+describe('Viewing a publisher profile.', function () {
+  it('Does stuff.', function () {
+    cy.visit('/person/1')
   })
 })
